b980488822947e9e2d850bc24f29434427e35c0f
Samuel-Webster-178 <samuel.webster@mths.ca>
2022-02-25 19:47:38 +0000

    commit initial

---
+++ circleMath.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Samuel Webster All rights reserved
+//
+// Created by Samuel Webster
+// Created on February 2022
+// with radius 10m
+
+#include <iostream>
+#include <cmath>
+
+
+int main() {
+    // I calculate and print the circumference and area
+    std::cout << "If a circle has a radius of 10m: " << std::endl;
+    std::cout << "Area is " << (M_PI * pow(15, 2)) << "m²." << std::endl;
+    std::cout << "Circumference is " << (2 * M_PI * 15) << "m." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
